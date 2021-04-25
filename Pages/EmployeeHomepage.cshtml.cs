00cb8cdc3bc10f31c240f8e7b79f3023a493c78e
Sydney <sydneyjberry12@gmail.com>
2021-04-25 17:11:22 -0500

    Created employee section of website

---
+++ Pages/EmployeeHomepage.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+
+namespace BITS_Project.Pages
+{
+    public class EmployeeHomepageModel : PageModel
+    {
+        public string Username { get; set; }
+
+        public void OnGet()
+        {
+            Username = HttpContext.Session.GetString("username");
+        }
+
+        public IActionResult OnGetLogout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToPage("Index");
+        }
+    }
+}
