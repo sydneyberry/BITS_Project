2c9fee26031d550d7922d81e9d0748c308a306ad
Sydney <sydneyjberry12@gmail.com>
2021-04-26 01:49:49 -0500

    Employee access for rentals and confirmation

--- Pages/Confirmation.cshtml.cs
+++ Pages/Confirmation.cshtml.cs
@@ -2,15 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BITS_Project.Pages
 {
+
     public class ConfirmationModel : PageModel
     {
+        public int SignedIn { get; set; }
+
         public void OnGet()
         {
+            if (HttpContext.Session.GetInt32("signed_in").GetValueOrDefault() == 0)
+            {
+                SignedIn = 0;
+            }
+            else
+            {
+                SignedIn = (int)HttpContext.Session.GetInt32("signed_in");
+            }
         }
     }
 }
--- Pages/Rentals/Details.cshtml.cs
+++ Pages/Rentals/Details.cshtml.cs
@@ -7,12 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BITS_Project.Data;
 using BITS_Project.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace BITS_Project.Pages.Rentals
 {
     public class DetailsModel : PageModel
     {
         private readonly BitsContext _context;
+        public int SignedIn { get; set; }
 
         public DetailsModel(BitsContext context)
         {
@@ -23,6 +25,15 @@ namespace BITS_Project.Pages.Rentals
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+            if (HttpContext.Session.GetInt32("signed_in").GetValueOrDefault() == 0)
+            {
+                SignedIn = 0;
+            }
+            else
+            {
+                SignedIn = (int)HttpContext.Session.GetInt32("signed_in");
+            }
+
             if (id == null)
             {
                 return NotFound();
